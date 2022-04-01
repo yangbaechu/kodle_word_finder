2e386dff0aae606080c905628eb06af270b67152
yangbaechu <root@ip-172-31-9-109.ap-northeast-2.compute.internal>
2022-04-01 14:56:49 +0000

    convert words df to django model

--- django_server/findersite/finder/models.py
+++ django_server/findersite/finder/models.py
@@ -4,3 +4,6 @@ class Word(models.Model):
     word_text = models.CharField(max_length=40)
     word_pieces = models.CharField(max_length=40)
     frequent = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.word_text
