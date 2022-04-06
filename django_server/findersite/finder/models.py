d9b4438ee2e15680d5e3cb1e81de5d3f9e61028a
yangbaechu <root@ip-172-31-9-109.ap-northeast-2.compute.internal>
2022-04-06 11:52:49 +0000

    change model

--- django_server/findersite/finder/models.py
+++ django_server/findersite/finder/models.py
@@ -1,17 +1,12 @@
 from django.db import models
 
-class Word(models.Model):
-    word_text = models.CharField(max_length=40)
-    word_pieces = models.CharField(max_length=40)
-    frequent = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.word_text
-
-#쿼리에 적용할 수 있는 함수
 class WordManager(models.Manager):
+    
+    #def get_queryset(self):
+        #return super().get_queryset().filter(word_pieces[3] = 'ㄱ')
+
     #노란색 단어
-    def yellow_letters(self, letters):
+    def yellow_letters(self, queryset, letters):
         
         def yellow_letter(letter, QuerySet):
             return QuerySet.filter(word_pieces__contains=letter)
@@ -19,6 +14,24 @@ class WordManager(models.Manager):
         QuerySet = Word.objects.all()
 
         for l in letters:
-            QuerySet = letter_exist(l, QuerySet)  
+            QuerySet = yellow_letter(l, QuerySet)  
 
         return QuerySet
+
+class Word(models.Model):
+    word_text = models.CharField(max_length=40)
+    word_pieces = models.CharField(max_length=40)
+    frequent = models.IntegerField(default=0)
+    letter1 = models.CharField(max_length=5, default='')
+    letter2 = models.CharField(max_length=5, default='')
+    letter3 = models.CharField(max_length=5, default='')
+    letter4 = models.CharField(max_length=5, default='')
+    letter5 = models.CharField(max_length=5, default='')
+    letter6 = models.CharField(max_length=5, default='')
+    
+    objects = WordManager()
+
+    def __str__(self):
+        return self.word_text
+
+
--- django_server/findersite/preprocessing.py
+++ django_server/findersite/preprocessing.py
@@ -46,13 +46,60 @@ six_letter_nouns = nouns.loc[is_six_letter,:].copy()
 final_words = six_letter_nouns.drop_duplicates(['word']).copy()
 final_words.fillna(3000, inplace=True)
 
+#글자별 column 만들기
+#final_words['word_pieces'] = list(final_words['word_pieces'])
+
+#시리즈 만들 배열
+index = []
+letter1 = []
+letter2 = []
+letter3 = []
+letter4 = []
+letter5 = []
+letter6 = []
+
+for row in final_words.iterrows():
+    index.append(row[0])
+    
+    letter1.append(row[1]['word_pieces'][0])
+    letter2.append(row[1]['word_pieces'][1])
+    letter3.append(row[1]['word_pieces'][2])
+    letter4.append(row[1]['word_pieces'][3])
+    letter5.append(row[1]['word_pieces'][4])
+    letter6.append(row[1]['word_pieces'][5])
+    
+    #print(row[0][0])
+    #letter1 = row['word_pieces'][0]
+    #index = row.index()
+    #print(f'letter_1: {letter_1}, index: {index}')
+    #if index == 2:
+        #break
+
+#시리즈 만들기
+final_words["letter1"] = pd.Series(data=letter1, index=index)
+final_words["letter2"] = pd.Series(data=letter2, index=index)
+final_words["letter3"] = pd.Series(data=letter3, index=index)
+final_words["letter4"] = pd.Series(data=letter4, index=index)
+final_words["letter5"] = pd.Series(data=letter5, index=index)
+final_words["letter6"] = pd.Series(data=letter6, index=index)
+
+
+
+#print(final_words.loc[3, 'word_pieces'][2])
+#final_words['letter_1'] = final_words['word_pieces'][3]
+#print(final_words.head())
+
 #df to model
+records = Word.objects.all()
+records.delete()
+
 word_records = final_words.to_dict('records')
 
-model_instances = [Word(word_pieces=record['word_pieces'], word_text=record['word'], frequent=record['ranking']) for record in word_records]
+
+model_instances = [Word(word_pieces=record['word_pieces'], word_text=record['word'], frequent=record['ranking'], letter1=['letter1'], letter2=['letter2'], letter3=['letter3'], letter4=['letter4'], letter5=['letter5'], letter6=['letter6']) for record in word_records]
 
 Word.objects.bulk_create(model_instances)
 
-print(Word.objects.filter(id=32))
-#print(final_words.iloc[:50, :])
+#print(Word.objects.filter(id=32))
+print(final_words.iloc[120:180, :])
 #print(len(final_words))
