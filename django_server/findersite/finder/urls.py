5264fa11046c9e4ea90f47285b8e0c0202b0ead8
yangbaechu <root@ip-172-31-9-109.ap-northeast-2.compute.internal>
2022-07-02 14:23:52 +0000

    view 출력 오류 해결

--- django_server/findersite/finder/urls.py
+++ django_server/findersite/finder/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 app_name = 'finder'
 urlpatterns = [
-            path('', views.SearchFormView.as_view(), name='search'),
+            url(r'^$', views.SearchFormView.as_view(), name='search'),
+            url(r'^result/$', views.IndexView.as_view(), name='result'),
             ]
--- django_server/findersite/finder/views.py
+++ django_server/findersite/finder/views.py
@@ -8,19 +8,33 @@ from finder.forms import SearchWordForm
 class SearchFormView(FormView):
     form_class = SearchWordForm
     template_name = "finder/word_list.html"
+    success_url = '/result/'
+    #def def get_success_url(self):
+        #return str()
 
     #입력 받은 변수 검색 
     def form_valid(self, form):
-        yellow = self.request.GET.get('yellow_letters', '')
-        #form.cleaned_data['yellow_letters']
-        gray = self.request.GET.get('gray_letters', '')
-        green = False
-        print(yellow, gray)
+         
+        success_url = '/result/'
         #green = form.cleaned_data['green_letters']
+        yellow = form.cleaned_data['yellow_letters']
+        gray = form.cleaned_data['gray_letters']
+       
+        #입력값 querystring으로 indexView에 전달
+        if yellow or gray:
+            self.success_url += "?"
+        if yellow:
+            self.success_url += ('yellow=' + yellow)
+        if yellow and gray:
+            self.success_url += ('&gray=' + gray)
+        if not yellow and gray:
+            self.success_url += ('gray=' + gray)
 
+        print(yellow, gray)
+        
+        ''' 
         word_list = Word.objects.all()
     
-        '''
         if yellow:
             word_list = Word.objects.yellow_letters(word_list, yellow)
         
@@ -29,32 +43,35 @@ class SearchFormView(FormView):
         
         if green:
             word_list = Word.objects.green_letters(word_list, green)
-        '''
+        
         context = {}
-        context['word_list'] = word_list
-        context['yellow_letters'] = yellow
-        context['gray_letters'] = gray
+        '''
+        #context['word_list'] = word_list
+        #context['yellow_letters'] = yellow
+        #context['gray_letters'] = gray
         
         return super().form_valid(form)
-        #return render(self.request, self.template_name, context)
+        #render(self.request, self.template_name, context)        
 
 class IndexView(generic.ListView):
-    yellow = ['ㄴ','ㄹ']
-    gray = ['ㅇ', 'ㅏ', 'ㅣ']
+    form_class = SearchWordForm
     green = ['', '', '', 'ㄱ', '', '']
 
     #기본적으로 데이터 보여주는 함수
     def get_queryset(self):
         
+        yellow = self.request.GET.get("yellow", '')
+        gray = self.request.GET.get("gray", '')
+        
         word_list = Word.objects.all()
     
-        if self.yellow:
-            word_list = Word.objects.yellow_letters(word_list, self.yellow)
+        if yellow:
+            word_list = Word.objects.yellow_letters(word_list, yellow)
         
-        if self.gray:
-            word_list = Word.objects.gray_letters(word_list, self.gray)
+        if gray:
+            word_list = Word.objects.gray_letters(word_list, gray)
         
-        if self.green:
-            word_list = Word.objects.green_letters(word_list, self.green)
+        #if self.green:
+            #word_list = Word.objects.green_letters(word_list, self.green)
         
         return word_list
