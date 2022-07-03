4a87a7cdae7167a537c799467b648ad1e6383afd
yangbaechu <root@ip-172-31-9-109.ap-northeast-2.compute.internal>
2022-07-03 10:51:42 +0000

    단어 검색 기능 구현

--- django_server/findersite/finder/forms.py
+++ django_server/findersite/finder/forms.py
@@ -1,5 +1,12 @@
 from django import forms
 
 class SearchWordForm(forms.Form):
-    yellow_letters = forms.CharField(label='Yellow Letters')
-    gray_letters = forms.CharField(label='Gray Letters')
+    yellow_letters = forms.CharField(label='Yellow Letters', required=False)
+    gray_letters = forms.CharField(label='Gray Letters', required=False)
+
+    green_letters_1 = forms.CharField(label='Green Letters_1', required=False)
+    green_letters_2 = forms.CharField(label='Green Letters_2', required=False)
+    green_letters_3 = forms.CharField(label='Green Letters_3', required=False)
+    green_letters_4 = forms.CharField(label='Green Letters_4', required=False)
+    green_letters_5 = forms.CharField(label='Green Letters_5', required=False)
+    green_letters_6 = forms.CharField(label='Green Letters_6', required=False)
--- django_server/findersite/finder/views.py
+++ django_server/findersite/finder/views.py
@@ -9,69 +9,80 @@ class SearchFormView(FormView):
     form_class = SearchWordForm
     template_name = "finder/word_list.html"
     success_url = '/result/'
-    #def def get_success_url(self):
-        #return str()
+    
+    #입력한 단어를 url 쿼리스트링에 추가
+    def make_success_url(self, yellow, gray, green):
+        #1. 단어를 letters에 모으기
+        letters = []
+        if yellow != '':
+            letters.append("yellow=" + ','.join(yellow))
+        if gray != '':
+            letters.append("gray=" + ','.join(gray))
 
+        for i, green_letter in enumerate(green):
+            if green_letter:
+                letters.append("green_" + str(i+1) + "=" + str(green_letter))
+        
+        #2. letters로 url 만들기
+        for i, letter in enumerate(letters):
+            if i == 0:
+                self.success_url += '?'
+            self.success_url += letter
+            if i != len(letter) - 1:
+                self.success_url += "&"
+        return
+    
     #입력 받은 변수 검색 
     def form_valid(self, form):
          
-        success_url = '/result/'
-        #green = form.cleaned_data['green_letters']
-        yellow = form.cleaned_data['yellow_letters']
-        gray = form.cleaned_data['gray_letters']
+        #사용자가 입력한 변수 전달받고 배열로 변경
+        yellow = form.cleaned_data['yellow_letters'].split(",")
+        gray = form.cleaned_data['gray_letters'].split(",")
+        
+        green = []
+        for i in range(6):
+            field_name = "green_letters_" + str(i+1)
+            if form.cleaned_data[field_name]:
+                green.append(form.cleaned_data[field_name])
+            else:
+                green.append('')
        
-        #입력값 querystring으로 indexView에 전달
-        if yellow or gray:
-            self.success_url += "?"
-        if yellow:
-            self.success_url += ('yellow=' + yellow)
-        if yellow and gray:
-            self.success_url += ('&gray=' + gray)
-        if not yellow and gray:
-            self.success_url += ('gray=' + gray)
-
-        print(yellow, gray)
-        
-        ''' 
-        word_list = Word.objects.all()
-    
-        if yellow:
-            word_list = Word.objects.yellow_letters(word_list, yellow)
-        
-        if gray:
-            word_list = Word.objects.gray_letters(word_list, gray)
-        
-        if green:
-            word_list = Word.objects.green_letters(word_list, green)
-        
-        context = {}
-        '''
-        #context['word_list'] = word_list
-        #context['yellow_letters'] = yellow
-        #context['gray_letters'] = gray
+        self.make_success_url(yellow, gray, green)
         
         return super().form_valid(form)
-        #render(self.request, self.template_name, context)        
 
 class IndexView(generic.ListView):
     form_class = SearchWordForm
-    green = ['', '', '', 'ㄱ', '', '']
+    gray = []
+    yellow = []
+    green = []
 
     #기본적으로 데이터 보여주는 함수
     def get_queryset(self):
         
-        yellow = self.request.GET.get("yellow", '')
-        gray = self.request.GET.get("gray", '')
+        #form에서 전한 데이터 받기
+        self.yellow = self.request.GET.get("yellow", '').split(',')
+        self.gray = self.request.GET.get("gray", '').split(',')
+        self.green = []
         
+        for i in range(6):
+            field = 'green_' + str(i+1)
+            if self.request.GET.get(field, '') == '':
+                self.green.append('')
+            else:
+                self.green.append(self.request.GET.get(field, ''))
+        
+        
+        print(self.yellow, self.gray, self.green)
         word_list = Word.objects.all()
     
-        if yellow:
-            word_list = Word.objects.yellow_letters(word_list, yellow)
+        if self.yellow:
+            word_list = Word.objects.yellow_letters(word_list, self.yellow)
         
-        if gray:
-            word_list = Word.objects.gray_letters(word_list, gray)
+        if self.gray:
+            word_list = Word.objects.gray_letters(word_list, self.gray)
         
-        #if self.green:
-            #word_list = Word.objects.green_letters(word_list, self.green)
+        if self.green:
+            word_list = Word.objects.green_letters(word_list, self.green)
         
         return word_list
