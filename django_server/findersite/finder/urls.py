f536247a19ed14bc65aab10b148b06fef1a15476
yangbaechu <root@ip-172-31-9-109.ap-northeast-2.compute.internal>
2022-07-07 14:51:51 +0000

    IndexView 제거하고 FormView에서 검색 결과 바로 출력

--- django_server/findersite/finder/urls.py
+++ django_server/findersite/finder/urls.py
@@ -6,5 +6,5 @@ from . import views
 app_name = 'finder'
 urlpatterns = [
             url(r'^$', views.SearchFormView.as_view(), name='search'),
-            url(r'^result/$', views.IndexView.as_view(), name='result'),
+            url(r'^result/$', views.SearchFormView.as_view(), name='result'),
             ]
--- django_server/findersite/finder/views.py
+++ django_server/findersite/finder/views.py
@@ -8,30 +8,7 @@ from finder.forms import SearchWordForm
 class SearchFormView(FormView):
     form_class = SearchWordForm
     template_name = "finder/word_list.html"
-    success_url = '/result/'
-    
-    #입력한 단어를 url 쿼리스트링에 추가
-    def make_success_url(self, yellow, gray, green):
-        #1. 단어를 letters에 모으기
-        letters = []
-        if yellow != '':
-            letters.append("yellow=" + ','.join(yellow))
-        if gray != '':
-            letters.append("gray=" + ','.join(gray))
-
-        for i, green_letter in enumerate(green):
-            if green_letter:
-                letters.append("green_" + str(i+1) + "=" + str(green_letter))
         
-        #2. letters로 url 만들기
-        for i, letter in enumerate(letters):
-            if i == 0:
-                self.success_url += '?'
-            self.success_url += letter
-            if i != len(letter) - 1:
-                self.success_url += "&"
-        return
-    
     #입력 받은 변수 검색 
     def form_valid(self, form):
          
@@ -46,47 +23,22 @@ class SearchFormView(FormView):
                 green.append(form.cleaned_data[field_name])
             else:
                 green.append('')
-       
-        self.make_success_url(yellow, gray, green)
         
-        return super().form_valid(form)
-
-class IndexView(generic.ListView):
-    #form_class = SearchWordForm
-    gray = []
-    yellow = []
-    green = []
-    
-    #context 변수 전달, 오버라이딩
-    def get_context_data(self, form=SearchWordForm):
-        return super().get_context_data(form=form)
-
-    #기본적으로 데이터 보여주는 함수
-    def get_queryset(self):
-        
-        #form에서 전한 데이터 받기
-        self.yellow = self.request.GET.get("yellow", '').split(',')
-        self.gray = self.request.GET.get("gray", '').split(',')
-        self.green = []
-        
-        for i in range(6):
-            field = 'green_' + str(i+1)
-            if self.request.GET.get(field, '') == '':
-                self.green.append('')
-            else:
-                self.green.append(self.request.GET.get(field, ''))
-        
-        
-        print(self.yellow, self.gray, self.green)
+        #입력된 단어 검색
         word_list = Word.objects.all()
     
-        if self.yellow[0] != '':
-            word_list = Word.objects.yellow_letters(word_list, self.yellow)
+        if yellow[0] != '':
+            word_list = Word.objects.yellow_letters(word_list, yellow)
         
-        if self.gray[0] != '':
-            word_list = Word.objects.gray_letters(word_list, self.gray)
+        if gray[0] != '':
+            word_list = Word.objects.gray_letters(word_list, gray)
         
-        if self.green:
-            word_list = Word.objects.green_letters(word_list, self.green)
+        if green:
+            word_list = Word.objects.green_letters(word_list, green)
         
-        return word_list
+        context = {'word_list': word_list, 'form':form}
+        
+        return render(self.request, self.template_name, context)
+
+
+
